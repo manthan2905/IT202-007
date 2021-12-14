6a60012aeed02c1435aac4dc1b89c42443ae3d4b
manthan2905 <mp876@njit.edu>
2021-12-14 17:25:33 -0500

    Problem1

--- lib/functions.php
+++ lib/functions.php
@@ -130,3 +130,58 @@ function get_url($dest)
     //handle relative path
     return $BASE_PATH . $dest;
 }
+
+function refresh_account_balance()
+{
+    if (is_logged_in()) {
+        //cache account balance via BGD_Bills_History history
+        $query = "UPDATE Transections set balance = (SELECT IFNULL(SUM(diff), 0) from Transections WHERE src = :src) where id = :src";
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        try {
+            $stmt->execute([":src" => get_user_account_id()]);
+        } catch (PDOException $e) {
+            flash("Error refreshing account: " . var_export($e->errorInfo, true), "danger");
+        }
+    }
+}
+
+function get_user_account_id()
+{
+    if (is_logged_in() && isset($_SESSION["user"]["account"])) {
+        return (int)se($_SESSION["user"]["account"], "id", 0, false);
+    }
+    return 0;
+}
+
+function changeBalance($bills, $reason, $src = -1, $dest = -1, $memo = "")
+{
+    //I'm choosing to ignore the record of 0 point transactions
+    if ($bills > 0) {
+        $query = "INSERT INTO Transections (act_src_id, act_dest_id, amount, memo) 
+            VALUES (:acs, :acd, :pc, :r,:m), 
+            (:acs2, :acd2, :pc2, :r, :m)";
+        //I'll insert both records at once, note the placeholders kept the same and the ones changed.
+        $params[":acs"] = $src;
+        $params[":acd"] = $dest;
+        $params[":m"] = $memo;
+        $params[":pc"] = ($bills * -1);
+
+        $params[":acs2"] = $dest;
+        $params[":acd2"] = $src;
+        $params[":pc2"] = $bills;
+        $db = getDB();
+        $stmt = $db->prepare($query);
+        try {
+            $stmt->execute($params);
+            //Only refresh the balance of the user if the logged in user's account is part of the transfer
+            //this is needed so future features don't waste time/resources or potentially cause an error when a calculation
+            //occurs without a logged in user
+            if ($src == get_user_account_id() || $dest == get_user_account_id()) {
+                refresh_account_balance();
+            }
+        } catch (PDOException $e) {
+            flash("Transfer error occurred: " . var_export($e->errorInfo, true), "danger");
+        }
+    }
+}
