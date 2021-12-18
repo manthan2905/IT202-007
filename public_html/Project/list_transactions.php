2cb91a5e53a4d389785520071605eac335c64ae9
manthan2905 <mp876@njit.edu>
2021-12-17 19:46:48 -0500

    extra

--- public_html/Project/list_transactions.php
+++ public_html/Project/list_transactions.php
@@ -63,8 +63,6 @@ if (isset($_POST["search"]) && !empty($query) && !empty($account_type)) {
                     <div><?php safer_echo($r["created"]); ?></div>
                 </div>
                 <div>
-                    <a type="button" href="test_edit_transactions.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
-                    <a type="button" href="test_view_transactions.php?id=<?php safer_echo($r['id']); ?>">View</a>
                 </div>
             </div>
         <?php endforeach; ?>
